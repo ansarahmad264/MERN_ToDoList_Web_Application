ca882c772e824f8656f17bfbd7fc3c4ebdfb7ef6
Ansar Ahmad <ahmadansar264@gmail.com>
2025-06-17 17:13:37 +0500

    Added password hashing, authorization and methods for generaing Tokens

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 import { Timestamp } from 'bson';
-import mongoose from 'mongoose';
+import mongoose, {schema} from 'mongoose';
+import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const userSchema = new mongoose.Schema(
   {
@@ -22,5 +24,46 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+//hash the password before saving if its modified
+userSchema.pre("save", function(next){
+  if(!this.isModified("password"))return next();
+
+  this.password = bcrypt.hash("password",10)
+  next();
+})
+
+//checks if the password entered by the user is correct
+userSchema.methods.isPasswordCorrect = async function(password){
+  return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id:this._id,
+      userName:this.username,
+      email:this.email
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id:this._id,
+      userName:this.username,
+      email:this.email
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+  ) 
+}
 
 export const User = mongoose.model('User', userSchema);
